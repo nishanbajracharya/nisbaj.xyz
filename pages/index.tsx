3f2deea070d76cb428455299b5619123e38a36a1
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2021-09-18 12:32:11 +0545

    Change favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,6 +60,11 @@ export default function Home() {
         <title>nisbaj.xyz</title>
         <meta name="description" content="Nisbaj's personal space" />
         <link rel="icon" href="/favicon.ico" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className="container mx-auto p-4 flex flex-col justify-center items-center min-h-screen">
